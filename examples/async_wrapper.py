00d5931b08155ca9bf4d528b821536b605f771ee
Giglium <fre095@gmail.com>
2025-10-01 21:05:44 +0200

    fix: linter

--- examples/async_wrapper.py
+++ examples/async_wrapper.py
@@ -1,6 +1,10 @@
+# jscpd:ignore-start
+# pylint: disable=missing-module-docstring,missing-function-docstring,duplicate-code,broad-exception-caught
+
 import asyncio
-from vinted_scraper import AsyncVintedWrapper
 from time import sleep
+
+from vinted_scraper import AsyncVintedWrapper
 
 
 async def main():
@@ -10,16 +14,17 @@ async def main():
 
 
 if __name__ == "__main__":
-    max_retries = 5
-    retries = 0
+    MAX_RETRIES = 5
+    retries: int = 0
 
     # retry multiple times since in the CI it sometimes fails due to much requests
-    while retries < max_retries:
+    while retries < MAX_RETRIES:
         try:
             asyncio.run(main())
         except Exception as e:
             retries += 1
-            if retries == max_retries:
+            if retries == MAX_RETRIES:
                 raise e
-            else:
-                sleep(2**retries)  # Waiting before retrying
+            sleep(2**retries)  # Waiting before retrying
+
+# jscpd:ignore-end
