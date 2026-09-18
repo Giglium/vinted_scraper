9b246d4f782b818631acc9c9d58ff2fb702aae68
Giglium <fre095@gmail.com>
2026-09-18 08:08:28 +0200

    docs: document the new attribute_ids
    Closes #224

--- src/vinted_scraper/_scraper.py
+++ src/vinted_scraper/_scraper.py
@@ -32,9 +32,10 @@ class VintedScraper(VintedWrapper):
                 - price_from: Minimum price
                 - price_to: Maximum price
                 - order: Sort order
-                - catalog_ids: Category IDs
-                - brand_ids: Brand IDs
-                - size_ids : Size IDs
+                - attribute_ids[catalog]: Category IDs (comma-separated;
+                - attribute_ids[status]: Condition IDs (comma-separated;
+                - attribute_ids[brand]: Brand IDs (comma-separated;
+                - attribute_ids[size]: Size IDs (comma-separated;
 
         Returns:
             List of VintedItem objects representing search results.
